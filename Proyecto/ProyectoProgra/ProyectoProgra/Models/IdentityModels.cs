6d6cfd23641bb62a0d9a0a40199762bf342729fe
José Julián Abarca Fallas <111944492+Vibexz0706@users.noreply.github.com>
2023-11-13 23:43:20 -0600

    LOGIN CON ROLES
    LOGIN CON ROLES

---
+++ Proyecto/ProyectoProgra/ProyectoProgra/Models/IdentityModels.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ProyectoProgra.Models
+{
+    // Para agregar datos de perfil del usuario, agregue más propiedades a su clase ApplicationUser. Visite https://go.microsoft.com/fwlink/?LinkID=317594 para obtener más información.
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string  Nombre { get; set; }
+        public bool Estado { get; set; }
+        public string FechaConexion { get; set; }
+
+
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser, string> manager)
+        {
+            // Tenga en cuenta que authenticationType debe coincidir con el valor definido en CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Agregar reclamaciones de usuario personalizadas aquí
+            return userIdentity;
+        }
+    }
+
+    public class Roles : IdentityRole
+    {
+        [Required]
+        public string Informacion { get; set; }
+        [Required]
+        public bool Estado { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Roles, string, IdentityUserLogin, IdentityUserRole, IdentityUserClaim>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
